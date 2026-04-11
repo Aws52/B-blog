80a05f9f722319091446229c65ceba86c730177b
Aws52 <aws2004damra@gmail.com>
2026-04-11 14:12:04 +0300

    initial repos interfaces

---
+++ blog/src/main/java/com/banana/blog/repositories/TagRepository.java
@@ -0,0 +1,11 @@
+package com.banana.blog.repositories;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.banana.blog.domain.entities.Tag;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+}
