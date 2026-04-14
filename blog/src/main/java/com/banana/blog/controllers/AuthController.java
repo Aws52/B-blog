0c82c9a1fee5ae7e8790f5ccefb79840d1b5ece8
Aws52 <aws2004damra@gmail.com>
2026-04-14 18:51:34 +0300

    authentication feature, and replaced some depricated methods after doing the initial implementation

---
+++ blog/src/main/java/com/banana/blog/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package com.banana.blog.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banana.blog.domain.dtos.AuthResponse;
+import com.banana.blog.domain.dtos.LoginRequest;
+import com.banana.blog.services.AuthenticationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest){
+        UserDetails userDetails = authenticationService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+
+        String tokenValue = authenticationService.generateToken(userDetails);
+        AuthResponse authResponse = AuthResponse.builder()
+                .token(tokenValue)
+                .expiresIn(86400) // Token expires in 24 hours
+                .build();
+        return ResponseEntity.ok(authResponse);
+    }
+    
+}
---
+++ blog/src/main/java/com/banana/blog/services/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,57 @@
+package com.banana.blog.services.impl;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.banana.blog.services.AuthenticationService;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final Long jwtExpiryMs = 86400000L; // 24 hours
+
+    @Override
+    public UserDetails authenticate(String email, String password) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password));
+        return userDetailsService.loadUserByUsername(email);
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
+                .signWith(getSigningKey(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
